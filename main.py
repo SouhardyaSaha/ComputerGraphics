d3b8b76002271d3f3423a80e6baddc14e5e7ed6c
Souhardya Saha <souhardyasaha98@gmail.com>
2023-01-16 22:37:26 +0600

    bresenham circle

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from circle_drawing.bresenham import draw_circle
 from line_drawing.bresenham import bresenham_line_draw
 
 
 def main():
     # dda()
-    bresenham_line_draw()
+    # bresenham_line_draw()
+    draw_circle()
 
 
 if __name__ == '__main__':
